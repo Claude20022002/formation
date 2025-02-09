718a6de451d377a660ffbc5f6ce8585139693dbe
Claude20022002 <clusamote@gmail.com>
2025-02-09 17:42:41 +0100

    pret

--- front-end/src/api/exemple.tsx
+++
@@ -1,5 +0,0 @@
-export const getExemple = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const data = await response.json();
-    return data;
-};
